7b781d49932b1f49dd0ba436419c2ddbc49bbf60
hiepho992 <dinhhiep211992@gmal.com>
2020-05-06 15:57:51 +0700

    [Thực hành] Hiển thị đầu ra

---
+++ hienthidaura.js
@@ -0,0 +1,12 @@
+function myfs1() {
+    alert('hello');
+}
+function myfs2() {
+    document.getElementById("demo").innerHTML = "This is a paragraph";
+}
+function myfs3() {
+    document.write("This is a paragraph");
+}
+function myfs4(){
+    console.log("This is a paragraph");
+}

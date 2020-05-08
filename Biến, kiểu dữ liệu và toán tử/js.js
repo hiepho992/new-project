035c1eb22433e555f52d86eeb4359233c03b1100
hiepho992 <dinhhiep211992@gmal.com>
2020-05-08 15:12:14 +0700

    Biến, kiểu dữ liệu và toán tử

---
+++ Biến, kiểu dữ liệu và toán tử/js.js
@@ -0,0 +1,22 @@
+let Physical = +prompt('Nhập điểm môn lý');
+let Chemistry = +prompt('Nhập điểm môn hóa');
+let Biological = +prompt('Nhập điểm môn Sinh');
+let TB = (Physical + Chemistry + Biological)/3;
+let Total = (Physical + Chemistry + Biological);
+alert('Điểm tổng' +" " + Total);
+alert('Điểm trung bình' + " " + TB);
+
+
+let Celsius = +prompt('Nhập độ C');
+const F = (9*Celsius)/5 + 32;
+alert('Độ F là' + F);
+
+
+let radius = +prompt('nhập bán kính');
+const PI = 3.14;
+let area = radius**2 * PI;
+let perimeter = 2*radius * PI;
+alert('Diện tích hình tròn là:' + area);
+alert('Chu vi hình tròn' + perimeter);
+
+

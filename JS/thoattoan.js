bf9e8132e802ecf24ff385e700cfb35e1f0483fe
hiepho992 <dinhhiep211992@gmal.com>
2020-05-08 14:15:29 +0700

    Khai báo và sử dụng biến

---
+++ JS/thoattoan.js
@@ -0,0 +1,17 @@
+let i = 10;
+f = 20.5;
+b = true;
+s = "Hà Nội";
+document.write("i = " + i + "<br/>");
+document.write("f = " + f + "<br/>");
+document.write("b = " + b + "<br/>");
+document.write("s = " + s + "<br/>");
+
+
+
+let width = 15;
+let height = 20;
+let area = width * height;
+document.write("area =" + area);
+
+
---
+++ JS/thuattoan1.js
@@ -0,0 +1,12 @@
+let a = +prompt('Nhập số thứ nhất');
+let b = +prompt('Nhập số thứ hai');
+let c = a%b;
+if (c==0){
+    alert("a bội số của b");
+}
+else{
+    alert("a không phải bội số của b");
+}
+
+
+

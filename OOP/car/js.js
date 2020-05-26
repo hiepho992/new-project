44ce4a0a0a81697ecc8b4ece7734862ae8846542
hiepho992 <dinhhiep211992@gmail.com>
2020-05-26 08:06:27 +0700

    OOP

---
+++ OOP/car/js.js
@@ -0,0 +1,75 @@
+let ctx = document.getElementById("mycanvas").getContext('2d');
+let img = document.getElementById("car");
+let x = 0;
+function Mycar(x, y, width, height, speed) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.speed = speed;
+}
+let car = new Mycar(50, 50, 30, 30, 2);
+let car1 = new Mycar(50, 50, 30, 30, 2);
+function drawcar() {
+    ctx.drawImage(img, car.x, car.y, car.width, car.height);
+}
+drawcar();
+
+
+function update() {
+    ctx.clearRect(0, 0, 400, 400);
+    drawcar();
+    requestAnimationFrame(update);
+
+    if ((car.x + car.width) >= mycanvas.width ||  car.x < 0){
+        alert("Game Over!");       
+    }
+}
+
+
+function moveRight() {
+    car.x += car.speed;
+    document.getElementById("car").src = "tank.png";
+
+}
+function moveLeft() {
+    car.x -= car.speed;
+    document.getElementById("car").src = "tank1.png";
+}
+function moveDown() {
+    car.y += car.speed;
+    document.getElementById("car").src = "tank3.png";
+}
+function moveUp() {
+    car.y -= car.speed;
+    document.getElementById("car").src = "tank2.png";
+}
+
+function speedX(){
+    car.speed = car.speed*2;
+}
+
+  
+function keyDown(e) {
+    console.log("keyDown -> e", e)
+    switch (e.key) {
+        case "ArrowRight":
+            moveRight();
+            break;
+        case "ArrowLeft":
+            moveLeft();
+            break;
+        case "ArrowDown":
+            moveDown();
+            break;
+        case "ArrowUp":
+            moveUp();
+            break;
+        case "Control":
+            speedX();
+            break;
+
+    }
+}
+document.addEventListener("keydown", keyDown);
+update();
